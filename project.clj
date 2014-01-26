b76a37bd6efac406aea9ce1f6ae1398a9e8c0a98
Jim Barritt <jim@planet-ix.com>
2014-01-26 19:24:58 +0000

    [jmdb] Fixing name of project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ureporter-simulator "0.1.0-SNAPSHOT"
+(defproject clj-logging-config "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
